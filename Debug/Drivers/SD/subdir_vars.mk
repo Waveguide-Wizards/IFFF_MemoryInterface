34b25e6a58e251ec9bfeacf95df43d5a4dfd5c22
mark_hinkle <mark.hinkle@lasp.colorado.edu>
2019-11-19 11:06:17 -0700

    Added gcode

--- Debug/Drivers/SD/subdir_vars.mk
+++ Debug/Drivers/SD/subdir_vars.mk
@@ -5,31 +5,22 @@
 SHELL = cmd.exe
 
 # Add inputs and outputs from these tool invocations to the build variables 
-CMD_SRCS += \
-../tm4c123gh6pm.cmd 
-
 C_SRCS += \
-../main.c \
-../tm4c123gh6pm_startup_ccs.c 
+../Drivers/SD/sdcard.c 
 
 C_DEPS += \
-./main.d \
-./tm4c123gh6pm_startup_ccs.d 
+./Drivers/SD/sdcard.d 
 
 OBJS += \
-./main.obj \
-./tm4c123gh6pm_startup_ccs.obj 
+./Drivers/SD/sdcard.obj 
 
 OBJS__QUOTED += \
-"main.obj" \
-"tm4c123gh6pm_startup_ccs.obj" 
+"Drivers\SD\sdcard.obj" 
 
 C_DEPS__QUOTED += \
-"main.d" \
-"tm4c123gh6pm_startup_ccs.d" 
+"Drivers\SD\sdcard.d" 
 
 C_SRCS__QUOTED += \
-"../main.c" \
-"../tm4c123gh6pm_startup_ccs.c" 
+"../Drivers/SD/sdcard.c" 
 
 
