23446e6f5b581a04948023a15edcc8c1fea0bf77
margalitnoa <noma5727@colorado.edu>
2019-12-02 11:45:26 -0600

    Added while loop to sdcard.c for initializing sd. Gets stuck in clean_name now

--- main.c
+++ main.c
@@ -1,30 +1,34 @@
-/*
- * main.c
- */
 #include "sdcard.h"
-#include <stdio.h>
 
 int main(void)
 {
-    startSSI3();
-    initialize_sd(SSI3);
-    cs_high(SSI3);
-    change_speed(SSI3);
-    cs_low(SSI3);
-    read_first_sector(SSI3);
-    read_disk_data(SSI3);
-    long next_cluster=get_root_dir_first_cluster();
-    do
-    {
-        next_cluster=list_dirs_and_files(next_cluster,LONG_NAME,GET_SUBDIRS,SSI3);
-    }while(next_cluster!=0x0FFFFFFF && next_cluster!=0xFFFFFFFF);
-    printf("\nDirs and files listed\n\n");
-    next_cluster=get_first_cluster(5);
-    do
-    {
-        next_cluster=open_file(next_cluster,SSI3);
+    /*The following is correct*/
+    startSSI1();
+    initialize_sd(SSI1);
+    cs_high(SSI1);
+    change_speed(SSI1);
+    cs_low(SSI1);
+    read_first_sector(SSI1);
+    read_disk_data(SSI1);
 
+    /*The cluster you get in the next line is the one of the root directory, even though the the only thing you have in the SD card is the file. In the acknowledgements section of the project's PDF you have some links where the filesystem is explained in case you want to learn more */
+    long next_cluster=get_root_dir_first_cluster();
+
+    /*Once you get the cluster of the root, directory, you need to execute the following in order to fill the structure with the data of the files. By the structure I mean what you can see defined on the top of the sdcard.c file with the name file_dir */
+    do
+    {
+        next_cluster=list_dirs_and_files(next_cluster,SHORT_NAME,NO_SUBDIRS,SSI1);
+    }while(next_cluster!=0x0FFFFFFF && next_cluster!=0xFFFFFFFF);
+
+    //Here you can ask for the cluster of the file to be opened. In this example I try to get the cluster of file in position 5 in my structure:
+    next_cluster=get_first_cluster(0); //Note, if you have only one file, it should be located in position 0
+
+
+    do
+    {
+        next_cluster=open_file(next_cluster,SSI1);
     }while(next_cluster!=0x0FFFFFFF && next_cluster!=0xFFFFFFFF);
 
     return 0;
 }
+
